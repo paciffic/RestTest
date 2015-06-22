6a6eb2c80fafbcdbae5b711453d609c42c33e12e
paciffic <paciffic@pacifficui-iMacRoom-2.local>
2015-06-22 21:36:49 +0900

    2nd modified sources

--- RestTest/ViewController.swift
+++ RestTest/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -18,6 +19,43 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var btnSend: UIButton!
+    @IBOutlet weak var tfMsg: UITextField!
+    @IBOutlet weak var tfContent: UITextField!
+    
+    @IBAction func btnSend_Pushed(sender: AnyObject) {
+        //tfMsg.text = "Button was pushed!!!"
+        
+    var request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:8888")!)
+            httpGet(request){
+                (data, error) -> Void in
+                if error != nil {
+                    println(error)
+                    self.tfMsg.text = error
+                } else {
+                    println(data)
+                    self.tfMsg.text = "success"
+                    self.tfContent.text = data
+                }
+        }
+    
+    }
+    
+    func httpGet(request: NSURLRequest!, callback: (String, String?) -> Void) {
+            var session = NSURLSession.sharedSession()
+            var task = session.dataTaskWithRequest(request){
+                (data, response, error) -> Void in
+                if error != nil {
+                    callback("", error.localizedDescription)
+                } else {
+                    var result = NSString(data: data, encoding:
+                        NSASCIIStringEncoding)!
+                    callback(result as String, nil)
+                }
+            }
+            task.resume()
     }
 
 

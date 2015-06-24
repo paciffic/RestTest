73578e18d6df1c575b767c1a85153926e2e5af1d
paciffic <paciffic@pacifficui-iMacRoom-2.local>
2015-06-24 20:17:50 +0900

    commit

--- RestTest/ViewController.swift
+++ RestTest/ViewController.swift
@@ -21,27 +21,77 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBOutlet weak var btnGet: UIButton!
     @IBOutlet weak var btnSend: UIButton!
-    @IBOutlet weak var tfMsg: UITextField!
-    @IBOutlet weak var tfContent: UITextField!
+    @IBOutlet weak var tfUsername: UITextField!
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfFirstName: UITextField!
+    @IBOutlet weak var tfLastName: UITextField!
+  
     
+  
     @IBAction func btnSend_Pushed(sender: AnyObject) {
+        print("Push Button is pressed!!")
+    }
+    
+    @IBAction func btnGet_Pushed(sender: AnyObject) {
         //tfMsg.text = "Button was pushed!!!"
         
-    var request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:8888")!)
-            httpGet(request){
-                (data, error) -> Void in
-                if error != nil {
-                    println(error)
-                    self.tfMsg.text = error
-                } else {
-                    println(data)
-                    self.tfMsg.text = "success"
-                    self.tfContent.text = data
-                }
-        }
+//    var request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:8888")!)
+//            httpGet(request){
+//                (data, error) -> Void in
+//                if error != nil {
+//                    println(error)
+//                    self.tfMsg.text = error
+//                } else {
+//                    println(data)
+//                    self.tfMsg.text = "success"
+//                    self.tfContent.text = data
+//                }
+//        }
     
+        process_json()
     }
+    
+    func process_json()
+    {
+        // URL 처리
+        let urlAsString = "http://localhost:8888"
+        let url = NSURL(string: urlAsString)!
+        let urlSession = NSURLSession.sharedSession()
+        
+        // connection task 처리
+        let jsonQuery = urlSession.dataTaskWithURL(url, completionHandler: { data, response, error -> Void in
+            if (error != nil) {
+                println(error.localizedDescription)
+            }
+            var err: NSError?
+            print(response)
+            
+            // JSON 데이터 parsing
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as! NSDictionary
+            if (err != nil) {
+                println("JSON Error \(err!.localizedDescription)")
+            }
+            print(jsonResult)
+            
+            // Dictionary에 담긴 데이터 변수로 할당
+            let jsonUsername: String? = jsonResult["username"] as? String
+            let jsonEmail: String? = jsonResult["email"] as? String
+            let jsonFistName: String? = jsonResult["firstName"]as? String
+            let jsonLastName: String? = jsonResult["lastName"] as? String
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                self.tfUsername.text = jsonUsername
+                self.tfEmail.text = jsonEmail
+                self.tfFirstName.text = jsonFistName
+                self.tfLastName.text = jsonLastName
+                
+            })
+        })
+        // web reques 시작
+        jsonQuery.resume()    }
     
     func httpGet(request: NSURLRequest!, callback: (String, String?) -> Void) {
             var session = NSURLSession.sharedSession()
